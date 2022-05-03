a9c3a2f21904590d7b98576d32d0e8b5d94bd528
EmirVildanov <reddog201030@gmail.com>
2022-05-03 18:20:21 +0300

    Finished theory task2

--- utils/utils.py
+++ utils/utils.py
@@ -1,2 +1,15 @@
 def print_task_info(info: str):
     print(info)
+
+
+if __name__ == "__main__":
+    with open("crash1.txt", "w") as f:
+        f.write("5\n")
+        f.write("1\n")
+        f.write("1\n")
+        f.write("2\n")
+        f.write("3\n")
+        q_num = 6
+        f.write(f"{q_num}\n")
+        for i in range(q_num):
+            f.write("2 3\n")
